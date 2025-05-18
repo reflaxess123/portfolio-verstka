629de76f81ad3320a91478f2d46efa2f0a6610d4
reflaxess123 <reflaxess@yandex.ru>
2025-05-18 17:28:05 +0300

    Добавлены новые стили для заголовка, включая адаптивные элементы и анимацию меню. Обновлены глобальные стили и добавлены новые константы для ширины контейнера. Изменены функции для адаптивного дизайна, чтобы поддерживать различные устройства.

--- src/widgets/Header/Header.tsx
+++ src/widgets/Header/Header.tsx
@@ -1,27 +1,47 @@
+import { useState } from 'react';
 import DownloadIcon from '../../shared/assets/svg/download.svg?react';
 import Logo from '../../shared/assets/svg/logo.svg?react';
+import MenuIcon from '../../shared/assets/svg/menu.svg?react';
 import { Button } from '../../shared/components/Button/Button';
 import styles from './Header.module.scss';
 
 export function Header() {
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
+
+  const handleMenuClick = () => {
+    setIsMenuOpen(!isMenuOpen);
+  };
+
   return (
-    <header className={styles['header']}>
-      <div className={styles['header__logo']}>
-        <Logo />
-        <h5 className={styles['header__logo-title']}>Personal</h5>
+    <div>
+      <header className={`${styles['header']} ${isMenuOpen ? styles.active : ''}`}>
+        <div className={styles['header__logo']}>
+          <Logo />
+          <h5 className={styles['header__logo-title']}>Personal</h5>
+        </div>
+        <ul className={styles['header__menu']}>
+          <h5 className={styles['header__menu-item']}>About Me</h5>
+          <h5 className={styles['header__menu-item']}>Skills</h5>
+          <h5 className={styles['header__menu-item']}>Projects</h5>
+          <h5 className={styles['header__menu-item']}>Contacts Me</h5>
+        </ul>
+        <Button
+          className={styles['header__button']}
+          icon={<DownloadIcon />}
+          size="medium"
+          text="Resume"
+        />
+        <MenuIcon className={styles['header__menu-icon']} onClick={handleMenuClick} />
+      </header>
+      <div className={`${styles['header__menu-overlay']} ${isMenuOpen ? styles.active : ''}`}>
+        <div className={styles['header__menu-overlay-items']}>
+          <h2 className={styles['header__menu-item']}>About Me</h2>
+          <h2 className={styles['header__menu-item']}>Skills</h2>
+          <h2 className={styles['header__menu-item']}>Projects</h2>
+          <h2 className={styles['header__menu-item']}>Contacts Me</h2>
+        </div>
+        <MenuIcon className={styles['header__menu-icon']} onClick={handleMenuClick} />
       </div>
-      <ul className={styles['header__menu']}>
-        <h5 className={styles['header__menu-item']}>About Me</h5>
-        <h5 className={styles['header__menu-item']}>Skills</h5>
-        <h5 className={styles['header__menu-item']}>Projects</h5>
-        <h5 className={styles['header__menu-item']}>Contacts Me</h5>
-      </ul>
-      <Button
-        className={styles['header__button']}
-        icon={<DownloadIcon />}
-        size="medium"
-        text="Resume"
-      />
-    </header>
+    </div>
   );
 }
